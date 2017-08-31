ff88af54e6b496390d291f122e883d298b4d7520
Tim Haak <tim@haak.co>
2017-08-31 15:19:05 +0200

    Make api endpoints relative.

--- src/app/constants/ConstantUrls.ts
+++ src/app/constants/ConstantUrls.ts
@@ -1,10 +1,10 @@
 export const ConstantUrls = {
-  allData: '/api/all',
-  asset_type: '/api/asset_type',
-  asset: '/api/asset',
-  asset_test: '/api/asset_test',
-  category: '/api/category',
-  category_capability: '/api/category_capability',
-  category_capability_level: '/api/category_capability_level',
-  config_upload: '/api/config_upload',
+  allData: './api/all',
+  asset_type: './api/asset_type',
+  asset: './api/asset',
+  asset_test: './api/asset_test',
+  category: './api/category',
+  category_capability: './api/category_capability',
+  category_capability_level: './api/category_capability_level',
+  config_upload: './api/config_upload',
 };
